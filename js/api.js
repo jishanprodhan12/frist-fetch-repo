02331fa57a6ef03dc1664340dce782cc21b87cc9
Jishan <mdjishanprodhan132@gamil.com>
2025-11-17 11:44:20 -0600

    add different way to write fetch

--- js/api.js
+++ js/api.js
@@ -1,1 +1,24 @@
-console.log("inside from js ")
+// fetch normal function 
+function loadUser() {
+    fetch('https://jsonplaceholder.typicode.com/users')
+        .then(res => res.json())
+        .then(data => console.log(data))
+        .catch(err => console.error('Error Happend ', err));
+}
+
+// fetch with arrow function 
+const loadPost = () => {
+    fetch('https://jsonplaceholder.typicode.com/posts')
+        .then(res => res.json())
+        .then(data => console.log(data))
+        .catch(err => console.error("ERROR Happend", err))
+}
+// fetch using async and await
+
+const loadComment = async() => {
+    const comments = await fetch('https://jsonplaceholder.typicode.com/comments');
+    const data = await comments.json();
+    console.log(data);
+}
+
+// fetch and try catch 
