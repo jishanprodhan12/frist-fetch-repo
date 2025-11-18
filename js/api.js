2215f9c9f6801613996021d26072abe0be09b237
Jishan <mdjishanprodhan132@gamil.com>
2025-11-18 10:41:17 -0600

    add title load

--- js/api.js
+++ js/api.js
@@ -15,24 +15,46 @@ const loadPost = () => {
 }
 // fetch using async and await
 
-const loadComment = async() => {
+const loadComment = async () => {
     const comments = await fetch('https://jsonplaceholder.typicode.com/comments');
     const data = await comments.json();
     console.log(data);
 }
 
 // fetch and try catch 
-const loadPhotos = async()=>{
-    try{
+const loadPhotos = async () => {
+    try {
         const photos = await fetch('https://jsonplaceholder.typicode.com/photos');
         const data = await photos.json();
         handlePhotos(data);
-    }catch(error){
-        console.error("error inside try catch and error is " , error);
+    } catch (error) {
+        console.error("error inside try catch and error is ", error);
     }
 }
- function handlePhotos(datas){
-    for(const data of datas){
-        console.log(data);
+function handlePhotos(datas) {
+    const container = document.getElementById('all-container');
+    for (const data of datas) {
+        const title = data.title ;
+        const div = document.createElement('div');
+        div.innerHTML = `
+        <div class="card bg-base-100 w-96 shadow-sm">
+        <figure>
+            <img   src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp" alt="Shoes" />
+        </figure>
+        <div class="card-body">
+            <h2 class="card-title">
+                ${title}
+                <div class="badge badge-secondary">NEW</div>
+            </h2>
+            <p>A card component has a figure, a body part, and inside body there are title and actions parts</p>
+            <div class="card-actions justify-end">
+                <div class="badge badge-outline">Fashion</div>
+                <div class="badge badge-outline">Products</div>
+            </div>
+        </div>
+    </div>
+    `
+        container.appendChild(div);
+   
     }
- }
+}
