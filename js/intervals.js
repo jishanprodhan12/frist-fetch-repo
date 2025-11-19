1168ac0c690c2197bbd89158fc0dae5fbd66b421
Jishan <mdjishanprodhan132@gamil.com>
2025-11-19 11:07:04 -0600

    set interval

--- js/intervals.js
+++ js/intervals.js
@@ -1,9 +1,8 @@
 console.log(1);
 console.log(2);
-const timeOutID = setTimeout(()=>{
-    console.log(3);
-},3000)
+setInterval(()=>console.log(3),3000)
 console.log(4);
 console.log(5);
 console.log(6);
-console.log(timeOutID);
+console.log(7);
+console.log(8);
---
+++ js/timeOuts.js
@@ -0,0 +1,9 @@
+console.log(1);
+console.log(2);
+const timeOutID = setTimeout(()=>{
+    console.log(3);
+},3000)
+console.log(4);
+console.log(5);
+console.log(6);
+console.log(timeOutID);
