bf534142e0a52a08259bd40c645e87ac7a41a2b6
Jishan <mdjishanprodhan132@gamil.com>
2025-11-19 11:11:01 -0600

    set interval

--- js/intervals.js
+++ js/intervals.js
@@ -1,8 +1,18 @@
-console.log(1);
-console.log(2);
-setInterval(()=>console.log(3),3000)
-console.log(4);
-console.log(5);
-console.log(6);
-console.log(7);
-console.log(8);
+// console.log(1);
+// console.log(2);
+// setInterval(()=>console.log(3),3000)
+// console.log(4);
+// console.log(5);
+// console.log(6);
+// console.log(7);
+// console.log(8);
+
+// objective 
+// 1. make a clock and counter 
+let num = 0 ;
+console.log(num);
+const clockId = setInterval(()=>{
+    num ++ ; 
+    console.log(num);
+},1000);
+
