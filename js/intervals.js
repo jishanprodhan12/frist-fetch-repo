518e82db1d201b402ce868349a5140e06946f85f
Jishan <mdjishanprodhan132@gamil.com>
2025-11-20 10:41:05 -0600

    make clock id

--- js/intervals.js
+++ js/intervals.js
@@ -9,15 +9,27 @@
 
 // objective 
 // 1. print all numbers using setInterval()
-let num = 0 ;
+let num = 0;
 console.log(num);
-const clockId = setInterval(()=>{
-    ++num ; 
-    if(num < 10){
-    console.log(num);
-    }else{
+const clockId = setInterval(() => {
+    ++num;
+    if (num < 10) {
+        console.log(num);
+    } else {
         clearInterval(clockId);
     }
-},1000);
+}, 1000);
+
+let counter = 0;
+console.log(counter);
+const counterId = setInterval(() => {
+    ++counter;
+    if (counter % 2 === 0 && counter <= 20) {
+        console.log("all even number's are between 0 to 20", counter);
+    }
+    clearInterval(counterId);
+}, 2000);
+
+console.log(counterId , clockId)
 
 
