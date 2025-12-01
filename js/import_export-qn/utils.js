dde3ce497f5353b3ffa038dee151b64f68633ae6
Jishan <mdjishanprodhan132@gamil.com>
2025-12-01 11:26:34 -0600

    import export

---
+++ js/import_export-qn/utils.js
@@ -0,0 +1,10 @@
+// utils.js
+
+function getVideoId(url) {
+    // এখানে ভিডিও ID বের করার লজিক থাকবে...
+    console.log("Extracting ID from:", url);
+    return 'video_id_123'; 
+}
+
+// ডিফল্ট এক্সপোর্ট সিনট্যাক্স
+export default getVideoId;
